97004046aef0069af7e298c949cd6886b3bf3d39
IMBA <privettt72@gmail.com>
2026-09-11 19:24:13 +0800

    feat: add invoice to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -70,6 +70,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
+        },
+    {
+      url: `${baseUrl}/invoice`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
