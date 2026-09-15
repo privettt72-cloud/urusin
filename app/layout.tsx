fa52c674ba466228bc0bc6264f69438f65fba15b
IMBA <privettt72@gmail.com>
2026-09-15 23:57:39 +0800

    chore: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,9 +84,9 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/icon.svg",
-    shortcut: "/icon.svg",
-    apple: "/icon.svg",
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
   },
 };
 
